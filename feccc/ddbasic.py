b9fbe5c0b7ee9545cb67b77028eeaaac87a47acb
o108minmin <o108minmin@asamikazuya-no-MacBook-Pro.local>
2016-07-03 16:24:08 +0900

    Add __truediv__ (dibasic)

--- feccc/ddbasic.py
+++ feccc/ddbasic.py
@@ -4,8 +4,10 @@
 from decimal import Decimal
 from feccc import roundfloat as rf
 from feccc.roundfloat import twosum, twoproduct
+from math import isinf
 
 
+# TODO: Debug for some numbers (inf, 0...)
 # TODO: Increase performance with numba
 
 
@@ -26,7 +28,6 @@ class DD:
     def __repr__(self):
         return str(self)
 
-
     def __pos__(self):
         return DD(self.high, self.low)
 
@@ -36,6 +37,7 @@ class DD:
     def __add__(self, arg):
         arg = DD.dd(arg)
         high, low = twosum(self.high, arg.high)
+        # TODO: Fix for inf
         low += self.low + arg.low
         high, low = twosum(high, low)
         return DD(high, low)
@@ -55,11 +57,43 @@ class DD:
     def __isub__(self, arg):
         return DD.__add__(self, -arg)
 
-    def __mul__(self, a):
-        pass
+    def __mul__(self, arg):
+        arg = DD.dd(arg)
+        high, low = twoproduct(self.high, arg.high)
+        # TODO: Fix for inf
+        low *= self.high * arg.low + self.low * arg.high + self.low * arg.low
+        high, low = twosum(high, low)
+        return DD(high, low)
 
-    def __truediv__(self, a):
-        pass
+    def __rmul__(self, arg):
+        return DD.__mul__(self, arg)
+
+    def __imul__(self, arg):
+        return DD.__mul__(self, arg)
+
+    def __truediv__(self, arg):
+        arg = DD.dd(arg)
+        high = self.high / arg.high
+        if isinf(high):
+            return DD(high, 0)
+        if isinf(arg.high):
+            return DD(high, 0)
+        h2, l2 = twoproduct(-high, arg.high)
+        if isinf(h2):
+            h2, l2 = twoproduct(-high, arg.high * 0.5)
+            low = ((((h2 + (self.high * 0.5)) - high * (arg.low * 0.5)
+                     ) + (self.low * 0.5)) + l2) / (arg.high * 0.5)
+        else:
+            low = ((((h2 + self.high) - high * arg.low) + self.low) + l2) / arg.high
+        h2, l2 = twosum(high, low)
+        return DD.dd(h2, l2)
+
+    def __rtruediv__(self, arg):
+        arg = DD.dd(arg)
+        return DD.__truediv__(arg, self)
+
+    def __itruediv__(self, arg):
+        return DD.__truediv__(self, arg)
 
     @staticmethod
     def dd(high=0, low=0):
