0535e021fcf8058993cec6bdceb05fd6b63a502c
o108minmin <o108minmin@asamikazuya-no-MacBook-Pro.local>
2016-07-03 15:30:09 +0900

    Fix __init__
    Fix for DD

--- feccc/__init__.py
+++ feccc/__init__.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from .roundmode import roundmode
+from .ddbasic import DD
 from feccc import roundfloat
+
+dd = DD.dd
 
 __all__ = (
     'roundmode',
-    'roundfloat'
+    'roundfloat',
+    'ddbasic'
 )
